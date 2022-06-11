4356324cc7391961bf27d812d3b1928cd1684d2d
Luv sahu <lksahu404@gmail.com>
2022-06-11 13:26:55 +0530

    This code for StopWatch

---
+++ Stopwatch/StopWatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    internal class StopWatch
+    {
+        public void Stop()
+        {
+            Console.WriteLine("Please Enter to Start");
+            Console.ReadLine();
+            DateTime Start = DateTime.Now;
+            Console.WriteLine("Please Enter to Stop");
+            Console.ReadLine();
+            DateTime End = DateTime.Now;
+            Console.WriteLine("Time Diffrance " + (End - Start));
+
+        }
+    }
+}
