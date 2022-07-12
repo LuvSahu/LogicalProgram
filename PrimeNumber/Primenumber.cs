67dc86b59d8d27c3356ee3e1c5413237ba984324
LuvSahu <104768173+LuvSahu@users.noreply.github.com>
2022-07-12 11:01:09 +0530

    Update Primenumber.cs

--- PrimeNumber/Primenumber.cs
+++ PrimeNumber/Primenumber.cs
@@ -8,18 +8,23 @@ namespace LogicalProblem
 {
     internal class Primenumber
     {
-            public void stopwatchprg()
+            public void PrimeNumberprg()
             {
-                Console.WriteLine("Press Enter to start");
-                Console.ReadLine();
-
-                DateTime Start = DateTime.Now;
-
-                Console.WriteLine("Press Enter to stop");
-                Console.ReadLine();
-
-                DateTime Stop = DateTime.Now;
-                Console.WriteLine("Elapsed Time = " + (Stop - Start));
+              int n, i, m=0, flag=0;    
+              Console.Write("Enter the Number to check Prime: ");    
+              n = int.Parse(Console.ReadLine());  
+              m=n/2;    
+              for(i = 2; i <= m; i++)    
+              {    
+               if(n % i == 0)    
+                {    
+                 Console.Write("Number is not Prime.");    
+                 flag=1;    
+                 break;    
+                }    
+              }    
+              if (flag==0)    
+               Console.Write("Number is Prime."); 
 
             }
         }
